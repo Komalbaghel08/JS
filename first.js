73f4713fa43fe176bfdafee6965a39b4f5979464
Komalbaghel08 <baghelkomal512@gmail.com>
2025-07-02 22:38:37 +0530

    new

---
+++ first.js
@@ -0,0 +1,2 @@
+console.log("welcome");
+document.write("<h1> hieeee </h1>");
---
+++ userinput/first.js
@@ -0,0 +1,48 @@
+// let name = prompt("Enter ur name")
+// console.log(name);
+
+// let num1 = parseInt(prompt("enter first num"))
+// let num2 = parseInt(prompt("enter second num"))
+// console.log(num1 +num2);
+
+
+// WAP To area of circle
+// let num = parseInt(prompt("enter"))
+// console.log(3.14*num*num);
+
+
+// WAP to swapping a number using third variable
+// let a = parseInt(prompt("enter first num"));
+// let b = parseInt(prompt("2nd num"));
+
+// console.log("a =", a);
+// console.log("b =", b);
+
+// let temp = a; 
+// a = b;
+// b = temp;
+
+// console.log("a =", a);
+// console.log("b =", b);
+
+
+// WAP to take one integer input from user
+
+// let b = parseFloat(prompt("Enter a number"));
+//  if (b>0){
+// console.log("positive");
+//  }
+//  else{
+//     console.log("negative");
+//  }
+
+
+let a = parseFloat(prompt("Enter a 1st number"));
+let b = parseFloat(prompt("Enter a 2nd number"));
+if(b%2 == 0){
+    console.log(a);
+
+}
+else{
+    console.log(a*b);
+}
