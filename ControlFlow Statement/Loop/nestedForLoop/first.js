03540b8248c6663d4cfd82eee6f543b06b3fced7
Komalbaghel08 <baghelkomal512@gmail.com>
2025-07-05 21:04:32 +0530

    update

--- ControlFlow Statement/Loop/nestedForLoop/first.js
+++ ControlFlow Statement/Loop/nestedForLoop/first.js
@@ -1,5 +1,17 @@
-for(let a=2; a<=3; a++){
-    for(let b =1; b < 11; b++){
-        console.log(a*b);
+// for(let a=2; a<=3; a++){
+//     for(let b =1; b < 11; b++){
+//         console.log(a*b);
+//     }
+// }
+
+for(let r = 1; r <= 5; r++){
+    for(let c = 1; c <=5; c++){
+        if(r == c || r + c == 6){
+            document.write('*')
+        }
+        else{
+            document.write('&nbsp&nbsp')
+        }
     }
+    document.write('<br>')
 }
