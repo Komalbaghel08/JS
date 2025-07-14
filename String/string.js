0796d80ee454aed65355efab208dcebd4994908c
Komalbaghel08 <baghelkomal512@gmail.com>
2025-07-14 16:20:35 +0530

    new

--- String/string.js
+++ String/string.js
@@ -1,8 +1,14 @@
-function Mummy(){
+function AI(){
 let str = "This is 1st String"
 let str1 = "This is 2nd  String"
 let str2 = "This is 3rd String"
 console.log(str.concat(str1,str2));
 // console.log(str.length);
 }
-Mummy()
+AI()
+
+
+// string me agr akhiri ka aik word htana h toh slice(0,-1)use krege 
+
+
+
---
+++ Template Literal/first.js
@@ -0,0 +1,15 @@
+let name = "Komal";
+let course = "Python Full Stack";
+
+let msg = `Hey ${name}, keep going strong in your ${course} journey!`;
+console.log(msg);
+
+
+// Multiline 
+let poem = `Roses are red,
+Violets are blue,
+You're coding in JS,
+And it's looking brand new!`;
+
+console.log(poem);
+
