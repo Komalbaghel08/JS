ed223c4512afbf42615d153ffb652abf6804a788
Komalbaghel08 <baghelkomal512@gmail.com>
2025-07-16 16:33:03 +0530

    update

---
+++ DOM/practice1.js
@@ -0,0 +1,16 @@
+const img1 = ()=> {
+    let i = document.querySelector(".main")
+    i.style.backgroundImage = "url(./gnesh.png)"
+}
+const img2 = ()=> {
+    let i = document.querySelector(".main")
+    i.style.backgroundImage = "url(./gnesh.png)"
+}
+const img3 = ()=> {
+    let i = document.querySelector(".main")
+    i.style.backgroundImage = "url(./gnesh.png)"
+}
+const img4 = ()=> {
+    let i = document.querySelector(".main")
+    i.style.backgroundImage = "url(./gnesh.png)"
+}
