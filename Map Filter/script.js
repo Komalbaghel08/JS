b5e02d3666c1ed19af632ce9c86b558a4f5fbf44
Komalbaghel08 <baghelkomal512@gmail.com>
2025-07-26 16:21:04 +0530

    new

---
+++ Map Filter/script.js
@@ -0,0 +1,20 @@
+let a = [2,4,5,6,7]
+let b = a.map( (e) => {
+ return e*2
+})
+console.log(b);
+
+
+let c = [2,3,4,5]
+let d = c.filter((e)=>{
+    return e>10
+    // return e%2 == 0
+})
+console.log(c);
+
+
+a.forEach((e)=>{
+    console.log(a*2);
+    })
+
+
