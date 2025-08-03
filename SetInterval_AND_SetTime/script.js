eb42a4c2fafe03b7e36eaed92f66154aef8cc1e8
Komalbaghel08 <baghelkomal512@gmail.com>
2025-08-03 21:11:50 +0530

    new

--- SetInterval_AND_SetTime/script.js
+++ SetInterval_AND_SetTime/script.js
@@ -1,7 +1,17 @@
-let okk=()=>{
-    setTimeout( ()=>{
+// let okk=()=>{
+//     setTimeout( ()=>{
 
-        alert("hii i am okkk")
-    }
-,3000)
-}
+//         alert("hii i am okkk")
+//     }
+// ,3000)
+// }
+
+
+// let c = 0;
+
+// function count() {
+//   setInterval(() => {
+//     console.log(c);
+//     c++;
+//   }, 20000);
+// }
