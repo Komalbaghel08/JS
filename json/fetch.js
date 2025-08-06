c82d5ce93be8db5f821036d096f57bd2b485e3cb
Komalbaghel08 <baghelkomal512@gmail.com>
2025-08-06 19:50:59 +0530

    new

---
+++ json/fetch.js
@@ -0,0 +1,8 @@
+let Fetchdata=async()=>{
+    let url='http://localhost:3000/hotel'
+
+    let res = await fetch(url,{method:"GET"})
+    let data = await res.json()
+    console.log(data);
+}
+Fetchdata()
